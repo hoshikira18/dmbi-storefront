70c336d3d8233d7227eb9e60c7f4e3e2ae77e473
vietnt1904 <thanhviet01092004@gmail.com>
2024-08-28 17:23:18 +0700

    fix: Delete test content

--- src/components/news/news-card.js
+++ src/components/news/news-card.js
@@ -7,7 +7,7 @@ const NewsItem = ({ news }) => {
     return (
         <Link
             href={`/news/${news.id}`}
-            className="mb-[30px] block min-h-[225px] rounded-md bg-white p-[15px] shadow pr-[3vw]"
+            className="mb-[30px] block min-h-[225px] rounded-md bg-white p-[15px] pr-[3vw] shadow"
         >
             <div className="sm:block md:flex">
                 <div className="mr-[15px] inline min-h-[200px] w-[30vw] items-center sm:max-h-[20vh] md:h-[20vh]">
@@ -23,21 +23,7 @@ const NewsItem = ({ news }) => {
                             {news.title}
                         </div>
                         <div className="max-h-[4.5em] overflow-hidden text-ellipsis text-justify md:max-h-[7.5em]">
-                            {news.description}
-                            <p className="inline">
-                                Hồ Chí Minh (chữ Nho: 胡志明; 19 tháng 5 năm
-                                1890 – 2 tháng 9 năm 1969), tên khai sinh là
-                                Nguyễn Sinh Cung (chữ Nho: 阮生恭), còn được
-                                biết với tên gọi Bác Hồ, là một nhà cách mạng và
-                                chính khách người Việt Nam. Ông là người sáng
-                                lập Đảng Cộng sản Việt Nam, từng là Chủ tịch
-                                nước Việt Nam Dân chủ Cộng hoà từ 1945–1969, Thủ
-                                tướng Việt Nam Dân chủ Cộng hòa trong những năm
-                                1945–1955, Tổng Bí thư Ban Chấp hành Trung ương
-                                Đảng Lao động Việt Nam từ 1956–1960, Chủ tịch
-                                Ban Chấp hành Trung ương Đảng Lao động Việt Nam
-                                từ năm 1951 cho đến khi qua đời.
-                            </p>
+                            <p className="inline">{news.description}</p>
                         </div>
                     </div>
                     <div className="absolute bottom-0 left-0 mt-[20px]">
